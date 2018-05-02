ef06abe24ec9bdaf9ade9a03ffa25d601824a6fa
Marcos Pivetta <marcospivetta60@gmail.com>
2018-05-02 16:21:12 -0300

    + Lojas modelo + Lojas Stack

---
+++ src/modelo/lojas.js
@@ -0,0 +1,14 @@
+export const lojas =
+    [
+        {
+            nome: "Saraiva",
+            telefone: {ddd: 51, numero: 32145578},
+            site: "saraiva.com.br",
+            endereco: {
+                text:
+                    {logradouro: "Rua das lamentações",bairro: "Bairro das lamentações",cidade: "Cidade das lamentações", pais: "País das lamentações"},
+                geo:
+                    {lat: 20.0, long: 20.0}
+            }
+        }
+    ]
--- src/navigation/LojasStack.js
+++ src/navigation/LojasStack.js
@@ -1,15 +1,32 @@
 import React from "react";
 import {StackNavigator} from 'react-navigation';
 import LojaListaScreen from "../screens/LojaListaScreen";
+import LojaScreen from "../screens/LojaScreen";
+import FormularioLojaScreen from "../screens/FormularioLojaScreen";
 
 export default StackNavigator({
 
-    ListaLojas: {
-      screen: LojaListaScreen,
-    },
-  },
+        Home: {
+            screen: LojaListaScreen,
+        },
 
-  {
-    initialRouteName: 'ListaLojas',
-  },
+        DetalhesLoja: {
+            screen: LojaScreen,
+        },
+
+        AdicionarLoja:{
+            screen: FormularioLojaScreen,
+            navigationOptions: {title: "Adicionar Loja"}
+        },
+
+        EditarLoja: {
+            screen: FormularioLojaScreen,
+            navigationOptions: {title: "Editar Loja"}
+        },
+
+    },
+
+    {
+        initialRouteName: 'Home',
+    },
 );
