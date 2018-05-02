80632aadcff4be081e877f8da02c60860712b19a
Marcos Pivetta <marcospivetta60@gmail.com>
2018-05-02 16:55:59 -0300

    + MenuBarLoja

---
+++ src/components/menuBarLoja.js
@@ -0,0 +1,73 @@
+import React from "react";
+import {MaterialIcons} from "@expo/vector-icons";
+import {StyleSheet, Platform, Text, TouchableOpacity, View} from "react-native";
+
+const MenuBarLoja = props => {
+
+  return (
+    <View style={styles.containerBarra}>
+      <Text style={Platform.OS === 'ios' ? styles.textoTituloIos : styles.textoTituloAndroid}>Livro</Text>
+
+      <View>
+        <TouchableOpacity
+          onPress={props.deletaItem}
+          style={styles.icone}>
+
+          <MaterialIcons
+            name={'delete'}
+            size={25}
+            color={'#fff'}/>
+
+        </TouchableOpacity>
+      </View>
+
+
+      <View>
+        <TouchableOpacity
+          onPress={props.editaItem}
+          style={styles.icone}>
+
+          <MaterialIcons
+            name={'edit'}
+            size={25}
+            color={'#fff'}/>
+
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({
+
+  containerBarra: {
+    flexDirection: 'row',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+
+  textoTituloIos: {
+    color: '#fff',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    paddingLeft: 30,
+    flex: 1,
+  },
+
+  textoTituloAndroid: {
+    color: '#fff',
+    fontWeight: 'bold',
+    flex: 1,
+    fontSize: 20,
+  },
+
+  icone: {
+    paddingRight: 10,
+    paddingLeft: 10,
+    justifyContent: 'center',
+  },
+
+});
+
+export default MenuBarLoja
